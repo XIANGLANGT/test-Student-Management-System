912a2517e2551cc826758208368dea43357b60f6
XIANGLANGT <133577303+XIANGLANGT@users.noreply.github.com>
2025-08-21 01:52:03 +0800

    更新：登录逻辑修正

---
+++ DAL/AdminService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class AdminService
+    {
+        /// <summary>
+        /// 根据用户账号和密码登录
+        /// </summary>
+        /// <param name="objAdmin">封装了登录账号和密码的用户对象/param>
+        /// <returns>返回包含用户信息的对象</returns>
+        public Admin AdminLogin(Admin objAdmin)    //用实体类Admin充当数据类型
+        {
+            //组合SQL语句
+            string sql = "select LoginId,LoginPwd,AdminName from Admins where loginId={0} and loginPwd='{1}'";
+            sql = string.Format(sql, objAdmin.LoginId, objAdmin.LoginPwd);
+            try
+            {
+                //从数据库中查询
+                SqlDataReader objReader = SQLHelper.GetReader(sql);
+                if (objReader.Read())      //如果objReader.Read()返回true则执行if内的语句
+                {
+                    objAdmin.AdminName = objReader["AdminName"].ToString();
+                    objReader.Close();
+                }
+                else
+                {
+                    objAdmin = null;  //如果登录不成功，则将当前对象清空
+                }
+            }
+            catch (SqlException)
+            {
+                throw new Exception("应用程序和数据库连接出现问题！");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return objAdmin;
+        }
+
+    }
+}
+
---
+++ DAL/DateValidate.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Text.RegularExpressions;
+
+namespace DAL
+{
+    /// <summary>
+    /// 通用验证类
+    /// </summary>
+    public class DateValidate
+    {
+        /// <summary>
+        /// 验证正整数
+        /// </summary>
+        /// <param name="txt"></param>
+        /// <returns></returns>
+        public static bool IsInteger(string txt)
+        {
+            //Regex objReg = new Regex(@"^[1-9]\d*$");
+            Regex objReg = new Regex(@"^\d+$");     // 验证非负整数（包括0）
+            //Regex objReg = new Regex(@"^-?\d+$");    // 验证整数（包括负数）
+            return objReg.IsMatch(txt);
+        }
+        /// <summary>
+        /// 验证是否是Email
+        /// </summary>
+        /// <param name="txt"></param>
+        /// <returns></returns>
+        public static bool IsEmail(string txt)
+        {
+            Regex objReg = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*");
+            return objReg.IsMatch(txt);
+        }
+        /// <summary>
+        /// 验证身份证
+        /// </summary>
+        /// <param name="txt">验证中国大陆身份证号码格式的正则表达式方法</param>
+        /// <returns></returns>
+        public static bool IsIdentityCard(string txt)
+        {
+            ////^\d{15}$：匹配15位纯数字的旧版身份证
+            ////^\d{18}$：匹配18位纯数字的新版身份证
+            ////^\d{17}(\d|X|x)$：匹配17位数字加最后一位数字或X/x的新版身份证
+            Regex objReg = new Regex(@"^(\d{15}$|^\d{18}$|^\d{17}(\d|X|x))$");
+            ////IsMatch()是Regex类提供的匹配方法，返回布尔值表示是否匹配成功‌
+            return objReg.IsMatch(txt);
+        }
+    }
+}
